b230fb9a8743e8abba084c3a4d2ccb218e61b962
Jacob McMichael <jacobmcmichael@gmail.com>
2022-10-15 16:29:22 -0700

    Add styles for icons under hero section
    Signed-off-by: Jacob McMichael <jacobmcmichael@gmail.com>

--- vue-ts/src/pages/Home.vue
+++ vue-ts/src/pages/Home.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="hero">
-    <p class="__header-style-4">Providing insightful research, analysis, and<br>resources to facilitate informed
-                                policy<br>decisions and
-                                administration of services</p>
+    <p class="__header-style-4">Providing insightful research, analysis, and resources to facilitate informed
+                                policy decisions and administration of services</p>
 
     <form id="search-form" class="search-form">
       <label></label>
@@ -10,12 +9,51 @@
       <button class="__button __solid" form="search-form" type="submit">Apply</button>
     </form>
   </div>
+
+  <div class="cards">
+    <div class="card" v-for="(card, index) in cardItems.hero" :key="index">
+      <img :src="card.img" :alt="card.alt" />
+      <p>{{ card.title }}</p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
+import Research from "../assets/research.svg";
+import Geography from "../assets/geography.svg";
+import Programs from "../assets/programs.svg";
+import Legislative from "../assets/legislative.svg";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      cardItems: {
+        hero: [
+          {
+            title: "Data & Research",
+            img: Research,
+            alt: "Data & Research",
+          },
+          {
+            title: "Geography & Mapping",
+            img: Geography,
+            alt: "Geography & Mapping",
+          },
+          {
+            title: "Programs & Services",
+            img: Programs,
+            alt: "Programs & Services",
+          },
+          {
+            title: "Legislative Fiscal Impacts",
+            img: Legislative,
+            alt: "Legislative Fiscal Impacts",
+          },
+        ],
+      },
+    };
+  },
 };
 </script>
 
