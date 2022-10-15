8431a2b06d561f606678a70ca2c6d4bbca39af32
Jacob McMichael <jacobmcmichael@gmail.com>
2022-10-15 11:24:51 -0700

    Add styles to navbar
    Signed-off-by: Jacob McMichael <jacobmcmichael@gmail.com>

---
+++ vue-ts/src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar">
+    <div class="menu">
+      <Menu />
+      <span>MENU</span>
+    </div>
+
+    <div class="row">
+      <div class="site-details-group">
+        <img src="../assets/scrfao-logo.png" alt="South Carolina Revenue and Fiscal Affairs Office" />
+
+        <div class="site-details">
+          <p>South Carolina<br>Revenue and Fiscal Affairs Office</p>
+          <p>Transforming data into solutions for South Carolina</p>
+        </div>
+      </div>
+
+
+      <div class="search-container">
+        <Search />
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import Menu from "./icons/Menu.vue";
+import Search from "./icons/Search.vue";
+
+export default {
+  name: "Navbar",
+  components: {
+    Menu,
+    Search,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../styles/components/_navbar";
+</style>
