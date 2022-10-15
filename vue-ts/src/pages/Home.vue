3119cdbe579f18a756bfd6766c32e15ce975e4bf
Jacob McMichael <jacobmcmichael@gmail.com>
2022-10-15 15:02:55 -0700

    Add styles for hero section
    - begin adding button styles
    
    Signed-off-by: Jacob McMichael <jacobmcmichael@gmail.com>

--- vue-ts/src/pages/Home.vue
+++ vue-ts/src/pages/Home.vue
@@ -1,18 +1,24 @@
 <template>
+  <div class="hero">
+    <p class="__header-style-4">Providing insightful research, analysis, and<br>resources to facilitate informed
+                                policy<br>decisions and
+                                administration of services</p>
 
+    <form id="search-form" class="search-form">
+      <label></label>
+      <input class="__text" placeholder="Search rfa.sc.gov" />
+      <button class="__button __solid" form="search-form" type="submit">Apply</button>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-import Navbar from "../components/Navbar.vue";
 
 export default {
   name: "Home",
-  components: {
-    Navbar,
-  },
 };
 </script>
 
-<style scoped>
-
+<style lang="scss">
+@import "../styles/pages/_home.scss";
 </style>
