82433383f028f6f3094e933910547f7eb5f09294
Jacob McMichael <jacobmcmichael@gmail.com>
2022-10-16 19:46:14 -0700

    Configures GitHub Actions to run deployment script
    - runs on closed pull request with main branch
    
    Signed-off-by: Jacob McMichael <jacobmcmichael@gmail.com>

--- vue-ts/vite.config.ts
+++ vue-ts/vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/portland-webworks-exercise/",
     plugins: [vue()],
     css: {
         preprocessorOptions: {
