a81dd66f91fa837469c5071c0d4fe1b3330e221d
Jacob McMichael <jacobmcmichael@gmail.com>
2022-11-02 17:37:36 -0700

    Adds responsive styles for dashboard section
    Signed-off-by: Jacob McMichael <jacobmcmichael@gmail.com>

--- src/components/PageHomeSectionDashboard.vue
+++ src/components/PageHomeSectionDashboard.vue
@@ -1,16 +1,16 @@
 <template>
-  <section id="dashboard">
-    <div class="container">
-      <div class="left-column">
-        <h2 class="__header-style-2">RFA Analytics - Public Dashboard</h2>
+  <section class="dashboard dashboard--flex">
+    <div class="dashboard__content">
+      <div class="dashboard__column">
+        <h1 class="dashboard__header">RFA Analytics - Public Dashboard</h1>
 
-        <div class="text-container">
-          <p class="__text">
+        <div class="dashboard__text-content">
+          <p class="dashboard__text">
             The S.C. Revenue and Fiscal Affairs Office offers online solutions for accessing publicly sourced data. This
             is accomplished through the use of a business intelligence dashboard tool and ArcGIS Server.
           </p>
 
-          <p class="__text">
+          <p class="dashboard__text">
             Our latest dashboards include population counts from the 2020 Census used for the redistricting process.
             Please note that gender and age are not included in this release.
             <a class="__link-style-2"
@@ -18,7 +18,7 @@
               Click here for population tables, downloadable files, and dashboards.</a>
           </p>
 
-          <p class="__text">
+          <p class="dashboard__text">
             Read more about RFA Online Analytics
             <a class="__link-style-2"
                href="https://rfa.sc.gov/data-services/online-analytics">here</a>.
@@ -26,11 +26,12 @@
         </div>
       </div>
 
-      <div class="right-column">
-        <div class="image-container">
-          <a href="https://rfa.sc.gov/data-research/population-demographics/census-state-data-center/decennial-census-data/decennial-0">
-            <button>
-              Click here to view census dashboards
+      <div class="dashboard__column">
+        <div class="dashboard__img-container">
+          <a class="dashboard__img-link"
+             href="https://rfa.sc.gov/data-research/population-demographics/census-state-data-center/decennial-census-data/decennial-0">
+            <button class="dashboard__button">
+              <span>View census dashboards</span>
             </button>
           </a>
         </div>
